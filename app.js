71532130f949427854324e718bd65900beeb5eb9
Rosie Waite <rosiewaite92@gmail.com>
2022-02-22 13:06:03 +0000

    event page and form is viewable

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// view engine setup
+
+app.get('/events/new', function(req, res){
+  res.render('events/new');
 });
+
+app.set('views');
+app.set('view engine', 'hbs');
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
 var homeRouter = require('./routes/home');
+var eventRouter = require('./routes/events')
 
 // route setup
 app.use('/', homeRouter);
+app.use('/new', eventRouter);
 
 module.exports = app;
