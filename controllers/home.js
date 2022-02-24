d543e582c0ed5edaaa55acfc22216485c84e4625
Jackie Benn <jackiebenn84@hotmail.com>
2022-02-24 10:11:54 +0000

    added code for user database to populate

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,21 @@
+const User = require('../models/user')
+
 var HomeController = {
   Index: function(req, res) {
     res.render('home/index', { title: 'Planet' });
-  }
-};
+  },
+  Create: function(req, res) {
+    var user = new User(
+      this.firstname = req.body.firstname,
+    this.surname = req.body.surname,
+    this.email = req.body.email,
+    this.password = req.body.password,
+    this.nickname = req.body.nickname,
+    this.dob = req.body.dob
+    );
+    console.log('hello')
+    user.save()
+  },
+}
 
 module.exports = HomeController;
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const app = require('../app');
 
 var HomeController = require('../controllers/home');
 
 router.get('/', HomeController.Index);
+router.post('/', HomeController.Create);
+
 
 module.exports = router;
